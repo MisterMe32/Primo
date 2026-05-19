9f7ccd4a6d27dc7f4f40b5b28877053a41c4f5a1
Jesse van Geelkerken <jessevangeelkerken@Jesses-MacBook-Air.local>
2026-05-19 21:52:43 +0200

    fixed chromium railway

--- src/commands/Vinted/electronics.js
+++ src/commands/Vinted/electronics.js
@@ -319,12 +319,14 @@ export default {
         );
 
         let firstRun = true;
-
-       const browser = await chromium.launch({
-   headless: true,
-   args: ['--no-sandbox', '--disable-setuid-sandbox']
+const browser = await chromium.launch({
+    executablePath: '/usr/bin/chromium-browser',
+    headless: true,
+    args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox'
+    ]
 });
-
         const page =
             await browser.newPage();
 
