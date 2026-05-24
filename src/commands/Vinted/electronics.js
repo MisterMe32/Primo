e10602eb61ba40308e449f623b34b448968799a1
Jesse van Geelkerken <jessevangeelkerken@Jesses-MacBook-Air.local>
2026-05-24 20:43:43 +0200

    fix place ai approved

--- src/commands/Vinted/electronics.js
+++ src/commands/Vinted/electronics.js
@@ -474,6 +474,19 @@ await page.setExtraHTTPHeaders({
 card.querySelector('a')
 ?.href || '';
 
+  console.log("AI APPROVED");
+    console.log("SENDING DISCORD MESSAGE");
+
+await interaction.channel.send({
+    content: `🔥 DEAL GEVONDEN
+
+📦 ${title}
+💰 €${price}
+🔗 ${link}`
+});
+
+console.log("DISCORD MESSAGE SENT");
+
                                     const image =
 card.querySelector('img')
 ?.src || '';
@@ -524,19 +537,6 @@ card.querySelector('img')
 
         continue;
     }
-
-    console.log("AI APPROVED");
-    console.log("SENDING DISCORD MESSAGE");
-
-await interaction.channel.send({
-    content: `🔥 DEAL GEVONDEN
-
-📦 ${title}
-💰 €${price}
-🔗 ${link}`
-});
-
-console.log("DISCORD MESSAGE SENT");
 }
 
                         // detectie
